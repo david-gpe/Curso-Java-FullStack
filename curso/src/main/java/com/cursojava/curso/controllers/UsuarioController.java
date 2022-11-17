72048cfed5b05f846ad429baf6908aa3882298b6
david-gpe <gpemaldavid@gmail.com>
2022-11-17 17:44:53 -0600

    Mejorando los UsiarioController, agregando el package models y su respectiva clase y remplazando la interfaz tables por usuarios

--- curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.cursojava.curso.controllers;
 
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,49 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value ="prueba")
-    public List<String> Prueba(){
-        return List.of("MAnzana","Kiwi","Platano");
+    @RequestMapping(value ="usuario/{id}")
+    public Usuario getUsuario(@PathVariable long id){
+        Usuario usuario = new Usuario();
+
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("2481314567");
+
+        return usuario;
+    }
+
+    @RequestMapping(value ="mod")
+    public Usuario mod(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("2481314567");
+
+        return usuario;
+    }
+
+    @RequestMapping(value ="eliminar")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("2481314567");
+
+        return usuario;
+    }
+
+    @RequestMapping(value ="buscar")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("lucasmoy@gmail.com");
+        usuario.setTelefono("2481314567");
+
+        return usuario;
     }
 }
